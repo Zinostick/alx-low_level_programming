46601c6868da4b96c974ad69309eadd76641716d
root <osauzogift@yahoo.com>
2022-03-11 18:05:38 -0800

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * mai - Prints the alphabet without q and e.
+ *
+ * Retuen: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i < 123; i++)
+{
+	if  (i != 101 && i != 113)
+{
+	putchar(i)
+}
+}
+        putchar('\n');
+	retuen (0);
+}
+
