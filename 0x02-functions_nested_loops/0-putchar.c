9e1fa10cd3464db4c97532fd2e9b6a8f16c79a3b
root <osauzogift@yahoo.com>
2022-03-16 06:51:21 -0700

    printing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-int main()
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
 {
-	int letter;
-	printf("enter or else");
-	getchar();
-	letter = '_';
-	putchar(letter);
-	letter = 'p';
-	putchar(letter);
-	letter = 'u';
-	putchar(letter);
-	letter = 't';
-	putchar(letter);
-	letter = 'c';
-	putchar(letter);
-	letter = 'h';
-	putchar(letter);
-	letter = 'a';
-	putchar(letter);
-	letter = 'r';
-	putchar(letter);
-	printf("\n");
-	return(0);
+                     _putchar('_');
+		     _putchar('p');
+		     _putchar('u');
+		     _putchar('t');
+		     _putchar('c');
+		     _putchar('h');
+		     _putchar('a');
+		     _putchar('r');
+		     _putchar('\n')
+
+		     return (0);
 }
