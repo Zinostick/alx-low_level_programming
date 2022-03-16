12885d47d9d10da47197745a9dafaec47ee3d363
root <osauzogift@yahoo.com>
2022-03-16 05:54:05 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int letter;
+	printf("enter or else");
+	getchar();
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+	printf("\n");
+	return(0);
+}
